6bc9bfce39ee5abf633cf903492a5162747844b1
Aynur Galimov <anrgl@ya.ru>
2021-11-15 22:55:54 +0500

    Add tests for post comments

--- test/controllers/post_comments_controller_test.rb
+++ test/controllers/post_comments_controller_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class PostCommentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'opens post page with comments' do
+    get post_path(posts(:one))
+
+    assert_response :success
+    assert_select 'p', 'Test comment content'
+  end
+
+  test 'create comment' do
+    post post_post_comments_path(posts(:one).id), params: { post_comment: { content: 'Test comment' } }
+
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_select 'p', 'Test comment'
+  end
 end
