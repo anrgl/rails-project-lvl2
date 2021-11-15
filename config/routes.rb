10ee60f461f401ddd4d6ad4aa607f46197c39d52
Aynur Galimov <anrgl@ya.ru>
2021-11-15 16:55:09 +0500

    Add post controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'home#index'
+  root 'posts#index'
+  resources :posts
 end
---
+++ test/controllers/posts_controller_test.rb
@@ -0,0 +1,34 @@
+require "test_helper"
+
+class PostsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  test 'opens all posts page' do
+    get root_path
+
+    assert_response :success
+    assert_select 'h1', 'Posts'
+  end
+
+  test 'opens post new page' do
+    sign_in users(:one)
+    get new_post_path
+
+    assert_response :success
+    assert_select 'h1', 'Create new post'
+    assert_select 'form', 1
+    assert_select 'input[type=text]', 1
+    assert_select 'textarea', 1
+    assert_select 'select', 1
+  end
+
+  test 'creates a new post' do
+    sign_in users(:one)
+    post posts_path, params: { post: { title: 'Test title', body: 'Test body', category_id: post_categories(:one).id } }
+
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_select 'h4', 'Test title'
+  end
+end
